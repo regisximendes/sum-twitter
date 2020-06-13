031f96a60ba7f2bf729ec0a51961ba096fb9a60a
Regis Ximendes <regisximendes.dev@gmail.com>
2020-06-13 20:47:12 -0300

    Feature: create login activity

---
+++ app/src/main/java/com/ximendes/sumtwitter/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.ximendes.sumtwitter.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ximendes.sumtwitter.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

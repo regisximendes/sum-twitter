1b30f2fc843e7c6ea6fa83dd670195ff5e93deaa
Regis Ximendes <regisximendes.dev@gmail.com>
2020-06-13 21:24:33 -0300

    Feature: setup view binding

--- app/src/main/java/com/ximendes/sumtwitter/ui/login/LoginActivity.kt
+++ app/src/main/java/com/ximendes/sumtwitter/ui/login/LoginActivity.kt
@@ -2,15 +2,29 @@ package com.ximendes.sumtwitter.ui.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.ximendes.sumtwitter.R
+import com.ximendes.sumtwitter.databinding.ActivityLoginBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginActivity : AppCompatActivity() {
 
     private val viewModel: LoginViewModel by viewModel()
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.viewModel = viewModel
+        viewListeners()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+
+    }
+
+    private fun viewListeners() {
+
     }
 }

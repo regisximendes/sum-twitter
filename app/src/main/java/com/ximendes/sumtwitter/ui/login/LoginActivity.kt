1bbd85c52a4bd542aaaa831a3a95183e6bd70969
Regis Ximendes <regisximendes.dev@gmail.com>
2020-06-21 00:26:41 -0300

    Feature: fix profile layout

--- app/src/main/java/com/ximendes/sumtwitter/ui/login/LoginActivity.kt
+++ app/src/main/java/com/ximendes/sumtwitter/ui/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.ximendes.sumtwitter.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -53,6 +54,7 @@ class LoginActivity : AppCompatActivity() {
                 viewModel.onSaveCredentials(oAuthCredential)
             }
             .addOnFailureListener {
+                Log.i("LOGIN ERROR","${it.message}")
                 viewModel.loginFail()
             }
     }
